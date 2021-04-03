a8ddf0e12676957b489eabcfcdc03293eb4f3bd3
David Boddie <david@boddie.org.uk>
2021-04-04 01:01:42 +0200

    Updated spim files for little-endian MIPS32.

--- Inferno/spim/include/u.h
+++ Inferno/spim/include/u.h
@@ -17,6 +17,8 @@ typedef unsigned int	u32int;		/* for /sys/include/libsec.h */
 typedef unsigned char u8int;
 typedef unsigned short u16int;
 typedef unsigned long uintptr;
+typedef unsigned int	u32int;
+typedef unsigned long long u64int;
 
 /* FCR */
 #define	FPINEX	(1<<7)
