34fa4f999c148fe980dd4914ec984769a09e47a7
David Boddie <david@boddie.org.uk>
2021-04-03 19:13:22 +0200

    Added missing file.

---
+++ os/nanonote/hardware.h
@@ -0,0 +1,81 @@
+#define CGU_CCR   0x10000000
+#define CGU_CLKGR 0x10000020
+
+enum CGUGates {
+    CGU_RTC = 4,
+    CGU_TCU = 2
+};
+
+#define RTC_BASE 0x10003000
+
+struct RTC {
+    ulong  control;
+    ulong  second;
+    ulong  second_alarm;
+    ulong  regulator;
+};
+typedef struct RTC RTC;
+
+#define INTERRUPT_BASE 0x10001000
+
+struct InterruptCtr {
+    ulong source;
+    ulong mask;
+    ulong mask_set;
+    ulong mask_clear;
+    ulong pending;
+};
+typedef struct InterruptCtr InterruptCtr;
+
+#define TIMER_BASE 0x10002010
+
+struct JZTimer {
+    ulong counter_enable;           /* TER */
+    ulong counter_enable_set;       /* TESR */
+    ulong counter_enable_clear;     /* TECR */
+    ulong stop;                     /* TSR */
+
+    ulong flag;                     /* TFR */
+    ulong flag_set;                 /* TFSR */
+    ulong flag_clear;               /* TFCR */
+    ulong stop_set;                 /* TSSR */
+
+    ulong mask;                     /* TMR */
+    ulong mask_set;                 /* TMSR */
+    ulong mask_clear;               /* TMCR */
+    ulong stop_clear;               /* TSCR */
+
+    ulong data_full0;               /* TDFR0 */
+    ulong data_half0;               /* TDHR0 */
+    ulong counter0;                 /* TCNT0 */
+    ulong control0;                 /* TCSR0 */
+    /* ... until TCSR7 */
+};
+typedef struct JZTimer JZTimer;
+
+enum TimerCounterEnable {
+    TimerCounter0 = 0x01,
+    TimerCounter1 = 0x02,
+    TimerCounter2 = 0x04,
+    TimerCounter3 = 0x08,
+    TimerCounter4 = 0x10,
+    TimerCounter5 = 0x20,
+    TimerCounter6 = 0x40,
+    TimerCounter7 = 0x80,
+    TimerCounterAll = 0xff
+};
+
+enum TimerPrescale {
+    TimerPrescale1 = 0,
+    TimerPrescale4 = 0x08,
+    TimerPrescale16 = 0x10,
+    TimerPrescale64 = 0x18,
+    TimerPrescale256 = 0x20,
+    TimerPrescale1024 = 0x28
+};
+
+enum TimerSource {
+    TimerSourceExt = 4,
+    TimerSourceRTC = 2,
+    TimerSourcePCLK = 1
+};
