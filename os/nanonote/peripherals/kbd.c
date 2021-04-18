f601f61a59da7231bf293d3ce2c7a3079f074228
David Boddie <david@boddie.org.uk>
2021-04-18 19:03:07 +0200

    Made the keyboard driver insert key codes into the keyboard queue.

--- os/nanonote/peripherals/kbd.c
+++ os/nanonote/peripherals/kbd.c
@@ -6,7 +6,44 @@
 
 #include "hardware.h"
 
+enum
+{
+    Spec    = 0x80,
+
+    View    = Spec|0x00,    /* view (shift window up) */
+    KF      = Spec|0x40,    /* function key */
+    Shift   = Spec|0x60,
+    Ctrl    = Spec|0x62,
+    Latin   = Spec|0x63,
+    Alt     = Latin,
+    Caps    = Spec|0x64,
+    No      = 0x00,
+
+    Up      = KF|14,
+    Vup     = KF|15,
+    Left    = View,
+    Right   = View,
+    Down    = View,
+    Vdown   = KF|16,
+    Del     = 0x7F,
+    Sym     = No,
+    Qi      = No,
+    Fn      = No,
+
+    Esc     = No
+};
+
 static kbd_column;
+static uchar kbd_state[8][8] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+};
 
 void kbdinit(void)
 {
@@ -41,6 +78,29 @@ void kbdinit(void)
 }
 
 static int kbd_c_values[8] = {10,11,12,13,14,15,16,17};
+static int kbd_d_values[8] = {18,19,20,21,22,23,24,26};
+
+static uchar keys[8][8] = {
+    { KF|1, KF|2, KF|3, KF|4, KF|5, KF|6,  KF|7, No    },
+    { 'q',  'w',  'e',  'r',  't',  'y',   'u',  'i'   },
+    { 'a',  's',  'd',  'f',  'g',  'h',   'j',  'k'   },
+    { Esc,  'z',  'x',  'c',  'v',  'b',   'n',  'm'   },
+    { '\t', Caps, '\\', '\'', ',',  '.',   '/',  Up    },
+    { 'o',  'l',  '=',  Sym,  ' ',  Qi,    Ctrl, 'l'   },
+    { KF|8, 'p',  Del,  '\n', Vup,  Vdown, Down, Right },
+    { Shift, Alt, Fn,   No,   No,   No,    No,   No    },
+};
+
+static uchar shift_keys[8][8] = {
+    { KF|1, KF|2, KF|3, KF|4, KF|5, KF|6,  KF|7, No    },
+    { 'Q',  'W',  'E',  'R',  'T',  'Y',   'U',  'I'   },
+    { 'A',  'S',  'D',  'F',  'G',  'H',   'J',  'K'   },
+    { Esc,  'Z',  'X',  'C',  'V',  'B',   'N',  'M'   },
+    { '\t', Caps, '\\', '\'', ',',  '.',   '/',  Up    },
+    { 'O',  'L',  '=',  Sym,  ' ',  Qi,    Ctrl, 'L'   },
+    { KF|8, 'P',  '\b', '\n', Vup,  Vdown, Down, Right },
+    { Shift, Alt, Fn,   No,   No,   No,    No,   No    },
+};
 
 void kbdpoll(void)
 {
@@ -52,13 +112,31 @@ void kbdpoll(void)
     GPIO *d_pin = (GPIO *)(GPIO_PORT_D_PIN | KSEG1);
 
     int bitfield = 1 << kbd_c_values[kbd_column];
-    c_func->clear = bitfield;
-    c_sel->clear = bitfield;
     c_dir->set = bitfield;
     c_data->clear = bitfield;
-    fbprint(d_pin->data & GPIO_Keyboard_In_Mask, 1 + kbd_column, 0x808000);
-    c_func->clear = bitfield;
-    c_sel->clear = bitfield;
+
+    for (int row = 0; row < 8; row++)
+    {
+        /* Check whether the key was pressed (bit goes low) */
+        uchar pressed = (d_pin->data & (1 << kbd_d_values[row])) ? 0 : 1;
+        uchar was_pressed = kbd_state[row][kbd_column];
+
+        kbd_state[row][kbd_column] = pressed;
+
+        if (row == 7 && (kbd_column == 0 || kbd_column == 1))  /* shift/alt */
+            continue;
+        else if (row == 5 && kbd_column == 6)   /* ctrl */
+            continue;
+
+        if (pressed && !was_pressed)
+        {
+            /* Keys that are pressed now, but weren't before */
+            if (kbd_state[7][0] != 0)
+                kbdputc(kbdq, shift_keys[row][kbd_column]);
+            else
+                kbdputc(kbdq, keys[row][kbd_column]);
+        }
+    }
     c_dir->clear = bitfield;
 
     kbd_column = (kbd_column + 1) & 7;
