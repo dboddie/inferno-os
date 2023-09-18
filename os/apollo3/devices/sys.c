15dd838ef9208b169166bdb3e8a187721e46a3e0
David Boddie <david@boddie.org.uk>
2023-09-18 02:55:33 +0200

    Add barrier instructions, as suggested.

--- os/apollo3/devices/sys.c
+++ os/apollo3/devices/sys.c
@@ -10,6 +10,7 @@ void enablefpu(void)
     /* Enable CP10 and CP11 coprocessors - see page 7-71 of the Arm Cortex-M4
        Processor Technical Reference Manual. */
     *(int *)CPACR_ADDR |= (0xf << 20);
+    coherence();
 }
 
 void disablefpu(void)
@@ -17,4 +18,5 @@ void disablefpu(void)
     /* Disable CP10 and CP11 coprocessors - see page 7-71 of the Arm Cortex-M4
        Processor Technical Reference Manual. */
     *(int *)CPACR_ADDR &= ~(0xf << 20);
+    coherence();
 }
