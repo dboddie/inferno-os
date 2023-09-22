a1789a5a7d705192c24f82ff0d8de0ce8eef850d
David Boddie <david@boddie.org.uk>
2023-09-22 18:30:13 +0200

    Added more debugging output when a panic occurs.

--- os/apollo3/dump.c
+++ os/apollo3/dump.c
@@ -9,6 +9,11 @@
 void dumpstack(void)
 {
     ulong sp = getsp();
+    if (up) {
+        print("up=%lux\n", up);
+        print("stack from %lux to %lux\n", (ulong)up->kstack, (ulong)up->kstack + KSTKSIZE);
+        print("sp=%lux\n", sp);
+    }
     for (int i = 0; i < 512; i+=4) {
         wrhex(*(int *)(sp + i));
         if (i % 16 == 12)
@@ -16,4 +21,7 @@ void dumpstack(void)
         else
             wrch(' ');
     }
+
+    poolsummary();
+    poolshow();
 }
