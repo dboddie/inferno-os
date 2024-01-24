940101ab9227ba6c2ab4d9aece9b9c2cd183f43d
David Boddie <david@boddie.org.uk>
2024-01-24 17:48:12 +0100

    Removed lots of commented code.

--- os/apollo3/trap.c
+++ os/apollo3/trap.c
@@ -29,8 +29,7 @@ void trapinit(void)
     /* Disable 8-byte stack alignment. */
     *(int *)CCR_ADDR &= ~CCR_STKALIGN;
 
-    /* Disable privileged access, use SP_main as the stack, disable FP
-       extension. */
+    /* Disable privileged access, use SP_main as the stack, disable FP extension. */
     setcontrol(0);
     disablefpu();
     *(int *)FPCCR_ADDR &= ~FPCCR_LSPEN;
@@ -86,14 +85,6 @@ void switcher(Ureg *ureg)
 {
     int t;
 
-//    wrstr("[switcher] > up="); wrhex((int)up); newline();
-//    wrstr("\x1b[1m@\x1b[m");
-//    wrstr(">> sp="); wrhex(getsp()); wrstr(" pc="); wrhex(*(ulong *)((ulong)ureg + 52)); newline();
-//    print("up=%.8lux\n", up);
-//    print("psr=%.8lux\n", ureg->psr);
-//    wrstr("in\r\n");
-//    dumpregs(ureg);
-
     if (up) up->pc = ureg->pc;
 
     t = m->ticks;       /* CPU time per proc */
@@ -112,13 +103,6 @@ void switcher(Ureg *ureg)
 
     if (rdch_ready())
         kbd_readc();
-
-//    print("up=%.8lux\n", up);
-//    wrstr("<< sp="); wrhex(getsp()); wrstr(" pc="); wrhex(*(ulong *)((ulong)ureg + 52)); newline();
-//    wrstr("pc="); wrhex((uint)ureg->pc); wrstr(" r14="); wrhex((uint)ureg->lr); newline();
-//    wrstr("out\r\n");
-//    dumpregs(ureg);
-//    wrstr("< up="); wrhex((int)up); newline();
 }
 
 void setpanic(void)
@@ -141,7 +125,6 @@ void kprocchild(Proc *p, void (*func)(void*), void *arg)
 {
     p->sched.pc = (ulong)linkproc;
     p->sched.sp = (ulong)p->kstack+KSTACK-8;
-//wrstr("kprocchild sp="); wrhex((int)p->sched.sp); newline();
     p->kpfun = func;
     p->arg = arg;
 }
@@ -150,41 +133,26 @@ void usage_fault(int sp)
 {
     /* Entered with sp pointing to an Ereg struct. */
     Ereg *er = (Ereg *)sp;
-//    wrstr("Usage fault at "); wrhex(er->pc); newline();
-/*
-    wrstr("CFSR="); wrhex(*(int *)CFSR_ADDR); newline();
-    wrstr("SHCSR="); wrhex(*(int *)SHCSR_ADDR); newline();
-    wrstr("FPCCR="); wrhex(*(int *)FPCCR_ADDR); newline();
-    wrstr("up="); wrhex((int)up); newline();
-    dumperegs(er);
-    dumpfpregs(er);
-*/
+
     if ((*(short *)UFSR_ADDR) & UFSR_UNDEFINSTR) {
         if (fpithumb2(er)) {
-/*
-            wrstr("CFSR="); wrhex(*(int *)CFSR_ADDR); newline();
-            wrstr("SHCSR="); wrhex(*(int *)SHCSR_ADDR); newline();
-            wrstr("FPCCR="); wrhex(*(int *)FPCCR_ADDR); newline();
-            wrstr("up="); wrhex((int)up); newline();
-*/
-/*
-            dumpfpregs(er);
-            newline();
-*/
             *(short *)UFSR_ADDR |= UFSR_UNDEFINSTR;
-//            setcontrol(CONTROL_FPCA);
-//            wrstr("control="); wrhex(getcontrol()); newline();
-//            wrstr("<-- "); wrhex(er->pc); newline();
             return;
         }
     }
 
     wrstr("Usage fault at "); wrhex((int)er->pc); newline();
     wrstr("UFSR="); wrhex(*(short *)UFSR_ADDR); newline();
+    wrstr("CFSR="); wrhex(*(int *)CFSR_ADDR); newline();
+    wrstr("SHCSR="); wrhex(*(int *)SHCSR_ADDR); newline();
+    wrstr("FPCCR="); wrhex(*(int *)FPCCR_ADDR); newline();
+    wrstr("up="); wrhex((int)up); newline();
 
     dumperegs((Ereg *)sp);
-    poolsummary();
-    poolshow();
+
+    for (int i = 0; i < 64; i+=4) {
+        wrhex(*(int *)(sp + i)); newline();
+    }
 
     for (;;) {}
 }
@@ -221,9 +189,10 @@ void hard_fault(int sp)
     wrstr("CONTROL="); wrhex(getcontrol()); newline();
     wrstr("intr enabled="); wrhex(islo()); newline();
     wrstr("up="); wrhex((int)up); newline();
+if (up) {
     wrstr("up->kstack="); wrhex((int)up->kstack); wrstr("..");
     wrhex((int)up->kstack + KSTKSIZE); newline();
-
+}
     dumperegs(er);
     dumpfpregs(er);
 
@@ -239,9 +208,6 @@ void hard_fault(int sp)
         else
             wrch(' ');
     }
-
-//    poolsummary();
-//    poolshow();
 
     for (;;) {}
 }
