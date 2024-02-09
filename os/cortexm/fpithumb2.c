d2ed8fbb9990b1e305e1c91f03161678be3df307
David Boddie <david@boddie.org.uk>
2024-02-09 22:59:49 +0100

    Fixed an issue where only the first task using floating point registers worked correctly; others had invalid register values.

--- os/cortexm/fpithumb2.c
+++ os/cortexm/fpithumb2.c
@@ -149,10 +149,10 @@ fpithumb2(Ereg *er)
     ufp = &up->env->fpu;
     if(ufp->fpistate != FPACTIVE) {
         ufp->fpistate = FPACTIVE;
-        ufp->control = 0;
-        ufp->status = (0x01<<28)|(1<<12);	/* software emulation, alternative C flag */
-        for (int n = 0; n < 16; n++)
-            er->s[n] = 0;
+//        ufp->control = 0;
+//        ufp->status = (0x01<<28)|(1<<12);	/* software emulation, alternative C flag */
+//        for (int n = 0; n < 16; n++)
+//            er->s[n] = 0;
     }
 
     ushort w0 = *(ushort *)er->pc;
